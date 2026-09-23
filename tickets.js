3836a76818173521e0b90539fca7ae1d3314be54
jonvincentdin <jonvincent.din@gmail.com>
2026-09-23 19:44:05 +0800

    Add 50% VIP surcharge for premium seating - din.jonvincent

--- tickets.js
+++ tickets.js
@@ -2,8 +2,8 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, isVIP = false) {
+  return Math.floor((quantity * basePrice) * (isVIP ? 1.5 : 1));
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
